d56a65f4222e137a358a1e1c825bf5681e530977
+ <+>
2023-06-21 00:25:04 +0300

    updated the controller and routes to test the seed data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,51 +1,55 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show update destroy ]
+  def seed_data
+    tasks = Task.all
+    render json: tasks
+  end
+  #before_action :set_task, only: %i[ show update destroy ]
+
+
 
   # GET /tasks
-  def index
-    @tasks = Task.all
+  #def index
+   ##render json: @tasks
+  #end
 
-    render json: @tasks
-  end
+  ## GET /tasks/1
+  #def show
+  #  render json: @task
+  #end
 
-  # GET /tasks/1
-  def show
-    render json: @task
-  end
+  ## POST /tasks
+  #def create
+  #  @task = Task.new(task_params)
 
-  # POST /tasks
-  def create
-    @task = Task.new(task_params)
+  #  if @task.save
+  #    render json: @task, status: :created, location: @task
+  #  else
+  #    render json: @task.errors, status: :unprocessable_entity
+  #  end
+  #end
 
-    if @task.save
-      render json: @task, status: :created, location: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
+  ## PATCH/PUT /tasks/1
+  #def update
+  #  if @task.update(task_params)
+  #    render json: @task
+  #  else
+  #    render json: @task.errors, status: :unprocessable_entity
+  #  end
+  #end
 
-  # PATCH/PUT /tasks/1
-  def update
-    if @task.update(task_params)
-      render json: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
+  ## DELETE /tasks/1
+  #def destroy
+  #  @task.destroy
+  #end
 
-  # DELETE /tasks/1
-  def destroy
-    @task.destroy
-  end
+  #private
+  #  # Use callbacks to share common setup or constraints between actions.
+  #  def set_task
+  #    @task = Task.find(params[:id])
+  #  end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.require(:task).permit(:title, :description, :completed)
-    end
+  #  # Only allow a list of trusted parameters through.
+  #  def task_params
+  #    params.require(:task).permit(:title, :description, :completed)
+  #  end
 end
